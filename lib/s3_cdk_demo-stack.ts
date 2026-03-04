020e1e87da45be8511e9d3c4b143b313ceacc2bc
Ragupathirajan Venkatesan <ragupathirajan.cloud@gmail.com>
2026-03-03 23:18:46 -0500

    Initial Commit

---
+++ lib/s3_cdk_demo-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+
+export class S3CdkDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'S3CdkDemoQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+    new s3.Bucket(this, 'MyPracticeBucket', {
+      bucketName: 'justs3-practice-bucket', // must be globally unique
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // safe for dev
+      autoDeleteObjects: true, // safe for dev
+    });
+
+  }
+}
---
+++ test/s3_cdk_demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3CdkDemo from '../lib/s3_cdk_demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3_cdk_demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3CdkDemo.S3CdkDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
